154a9a745cca3a078952ecc23c1882e5032ee5af
Collin DiMaio <cdimai6@wgu.edu>
2025-12-24 18:53:48 -0500

    add convex db query and mutaions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router"
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Hello World.</Text>
