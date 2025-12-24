bddf21cf1bf3cfdc0de6ca9ecaff1baa53082d67
Collin DiMaio <cdimai6@wgu.edu>
2025-12-24 09:35:24 -0500

    first commit project layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from 'expo-router'
+import { View, Text } from 'react-native'
+import {Ionicons,} from "@expo/vector-icons"
+import React from 'react'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    <Tabs screenOptions={{
+        tabBarActiveTintColor: "orange",
+        tabBarInactiveTintColor: "grey",
+        tabBarStyle: {
+            backgroundColor: "#090e21"
+        },
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    }}>
+        <Tabs.Screen 
+            name='index'
+            options={{
+                title: "Todos",
+                tabBarIcon: ({color,size}) => (
+                    <Ionicons name="flash-outline" size={size} color={color} />
+                )
+            }}
+        />
+        <Tabs.Screen 
+            name='settings'
+            options={{
+                title: "Settings",
+                tabBarIcon: ({color,size}) => (
+                    <Ionicons name="settings" size={size} color={color} />
+                )
+            }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default TabsLayout
