2268a1b9995043d24f335206991000727ebb5ee3
Collin DiMaio <cdimai6@wgu.edu>
2025-12-28 07:39:42 -0500

    populate settings.tsx and create ProgressStats component

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+//import DangerZone from "@/components/DangerZone";
+//import Preferences from "@/components/Preferences";
+import ProgressStats from "@/components/ProgressStats";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 const settings = () => {
+
+  const { colors } = useTheme();
+  const settingsStyles = createSettingsStyles(colors);
+
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
+
+  //const {colors, isDarkMode, toggleDarkMode}=useTheme();
+
   return (
-    <View>
-      <Text>settings</Text>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+              <Ionicons name="settings" size={28} color="#ffffff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+
+        <ScrollView
+          style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
 
